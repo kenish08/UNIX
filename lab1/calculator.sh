2a06d8786627028d7e0d2b4c08bcc095ecfdcedb
kenish-1BM18CS045 <53857041+kenish08@users.noreply.github.com>
2020-09-24 15:09:42 +0530

    Add files via upload

---
+++ lab1/calculator.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#Shell Sample Script
+echo "Type the two numbers"
+read a
+read b
+echo "Add : $(($a+$b))"
+echo "SUB : $(($a-$b))"
+echo "MUL : $(($a*$b))"
+echo "DIV : $(($a/$b))"
+echo "mod : $(($a%$b))"
---
+++ lab1/first.sh
@@ -0,0 +1,6 @@
+#!bin/sh
+#sample Shell Script
+echo "today's date: `date`"
+echo "My Shell: $Shell"
+echo "This month Cal: `cal`"
+echo "today's date: `date "+%d%m%Y"`"
