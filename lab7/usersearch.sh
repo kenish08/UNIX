c1e59476895dcd136f7d1dc51d681a15bcc91192
kenish-1BM18CS045 <53857041+kenish08@users.noreply.github.com>
2020-11-26 15:13:16 +0530

    Update usersearch.sh

--- lab7/usersearch.sh
+++ lab7/usersearch.sh
@@ -1,2 +1,15 @@
-echo "List of home directories: "
-grep -v "nologin" /etc/passwd | cut -d ":" -f 6 
+#! /bin/sh
+if test $# -eq 0; then
+    echo "No user provided"
+else
+    user_name=$1 
+    cat /etc/passwd | grep ${user_name} > /dev/null
+    if [ $? -eq 0 ] ; then
+        # User exists
+        printf "User Exists.\nHome directory of user: "
+        grep $user_name /etc/passwd | cut -d ":" -f 6 
+    else
+        # User doesn't exist
+        echo "User Not Found"
+    fi
+fi
